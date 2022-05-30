769aab8cb38a2955fc48ed3cf621a9419c937c25
78redok <81646320+78redok@users.noreply.github.com>
2022-05-29 17:17:12 -0700

    admin page update

--- client/src/App.js
+++ client/src/App.js
@@ -10,33 +10,47 @@ import TutorialsList from "./components/tutorials-list.component";
 class App extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/tutorials"} className="navbar-brand">
-            MyApp
-          </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/tutorials"} className="nav-link">
-                Tutorials
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Add
-              </Link>
-            </li>
-          </div>
-        </nav>
+      // <div>
+      //   <nav className="navbar navbar-expand navbar-dark bg-dark">
+      //     <Link to={"/tutorials"} className="navbar-brand">
+      //       MyApp
+      //     </Link>
+      //     <div className="navbar-nav mr-auto">
+      //       <li className="nav-item">
+      //         <Link to={"/tutorials"} className="nav-link">
+      //           Tutorials
+      //         </Link>
+      //       </li>
+      //       <li className="nav-item">
+      //         <Link to={"/add"} className="nav-link">
+      //           Add
+      //         </Link>
+      //       </li>
+      //     </div>
+      //   </nav>
 
-        <div className="container mt-3">
-          <Switch>
-            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
-            <Route exact path="/add" component={AddTutorial} />
-            <Route path="/tutorials/:id" component={Tutorial} />
-          </Switch>
+      //   <div className="container mt-3">
+      //     <Switch>
+      //       <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+      //       <Route exact path="/add" component={AddTutorial} />
+      //       <Route path="/tutorials/:id" component={Tutorial} />
+      //     </Switch>
+      //   </div>
+      // </div>
+
+<body>
+    <header>
+        <nav>
+            <img src={require("./mainlogo.png")} alt="logo"></img>
+        </nav>
+    </header>
+
+    <section id="mainintroduction">
+        <div class="about_container">
+            <h1>Admin Control</h1>
         </div>
-      </div>
+    </section>
+</body>
     );
   }
 }
